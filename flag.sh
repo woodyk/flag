88cc05cc07d72fe2c0bc4df6b5ae280b2644b6c1
Kato <kato@smallroom.com>
2024-11-02 22:41:57 -0400

    added little details

--- flag.sh
+++ flag.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Author: Wadih Khairallah
+# Date: 9/11/2001
+
 printf "\033[1;44m *   *   *   *   *   *  \033[1;41m                                                  \033[m\n"
 printf "\033[1;44m   *   *   *   *   *    \033[1;41m                                                  \033[m\n"
 printf "\033[1;44m *   *   *   *   *   *  \033[m\n"
