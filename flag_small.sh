ccc27e50daaf9d3679c45cb06e613807ae39b132
Kato <kato@smallroom.com>
2024-11-02 23:09:47 -0400

    created a smaller version

---
+++ flag_small.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Author: Wadih Khairallah
+# Date: 11/2/2024
+
+printf "\033[1;44m* * * * * *\033[1;41m                            \033[m\n"
+printf "\033[1;44m * * * * * \033[m\n"
+printf "\033[1;44m* * * * * *\033[1;41m                            \033[m\n"
+printf "\033[1;44m * * * * * \033[m\n"
+printf "\033[1;44m* * * * * *\033[1;41m                            \033[m\n"
+printf "\033[m\n"
+printf "\033[1;41m                                       \033[m\n"
+printf "\033[m\n"
+printf "\033[1;41m                                       \033[m\n"
+printf "\033[m\n"
+printf "\033[1;41m                                       \033[m\n"
+printf "\033[m\n"
+printf "\033[1;41m                                       \033[m\n"
+printf "\n"
+exit
